cf3a0ae6db95ce275f40c2f7d361e372ca66eed9
avnanda <anoop.v.nanda.21@dartmouth.edu>
2020-04-04 04:40:24 -0400

    Updating County to HRR
    Updated the County to HRR script to use the .csv from the NYT's github repo as the data source. Included outputs

--- countytohrr.R
+++ countytohrr.R
@@ -1,6 +1,6 @@
 # Anoop Nanda
 # April 2020
-# Converting County Level Covid-19 data to HRRs
+# Converting county Level Covid-19 data to HRRs
 
 # Loading Package libraries
 library(readr)
@@ -11,19 +11,19 @@ library(stringr)
 # loading and modifying data
 geocorr2014 <- read_csv("geocorr2014.csv") # loads crosswalk 
 geocorr2014$cntyname = str_remove(geocorr2014$cntyname, "Parish") # removes Parish from Lousiana county names
-
-nyt = read_csv("nyt.csv") # loads nytimes data
-nyt$STATE = setNames(state.abb, state.name)[nyt$STATE] #abbreviates states
-nyt$combined = paste(nyt$COUNTY,nyt$STATE) # creates county-state combo column, for merging
-
+nyt = read_csv("us-counties.csv") # loads nytimes data
+nyt = subset(nyt, nyt$date == max(nyt$date))
+nyt$state = setNames(state.abb, state.name)[nyt$state] #abbreviates states
+nyt$fips = str_pad(nyt$fips, 5, pad = 0) # pads 0s onto FIPS codes, for merging
+nyt <- subset(nyt, select = -c(date))
 # NYC and KC cases and deaths aren't separated out by county, grabbing those numbers for the nyt data
-NYCcases = nyt$CASES[which(nyt$COUNTY == "New York City")]
-NYCdeaths = nyt$DEATHS[which(nyt$COUNTY == "New York City")]
-KCcases = nyt$CASES[which(nyt$COUNTY == "Kansas City")]
-KCdeaths = nyt$DEATHS[which(nyt$COUNTY == "Kansas City")]
+NYCcases = nyt$cases[which(nyt$county == "New York City")]
+NYCdeaths = nyt$deaths[which(nyt$county == "New York City")]
+KCcases = nyt$cases[which(nyt$county == "Kansas City")]
+KCdeaths = nyt$deaths[which(nyt$county == "Kansas City")]
 
 # merging crosswalk and NYT data
-full = merge(geocorr2014, nyt, by.x = "cntyname", by.y = "combined", all.x = TRUE)
+full = merge(geocorr2014, nyt, by.x = "county", by.y = "fips", all.x = TRUE)
 full[is.na(full)]=0 
 full$pop10 <- as.numeric(full$pop10)
 full$afact = as.numeric(full$afact)
@@ -40,14 +40,14 @@ full2 = full1 %>%
 # NYC and KC data modifications - grabbing data for the metro regions, replacing empty county columns in the merged (full) dataset 
 NYC = subset(full2, county == 36005 | county == 36047 | county == 36061 | county == 36081 | county == 36085)
 NYC$countypop = sum(NYC$pop10)
-NYC$CASES = sum(NYC$CASES)+NYCcases
-NYC$DEATHS = sum(NYC$DEATHS)+NYCdeaths
+NYC$cases = sum(NYC$cases)+NYCcases
+NYC$deaths = sum(NYC$deaths)+NYCdeaths
 NYC$afact = NYC$pop10/NYC$countypop # New allocation factor to account for the merging of the boroughs (counties)
 
 KC = subset(full2, county == 29047 | county == 29037 | county == 29095 | county == 29165)
 KC$countypop = sum(KC$pop10)
-KC$CASES = sum(KC$CASES)+KCcases
-KC$DEATHS = sum(KC$DEATHS)+KCdeaths
+KC$cases = sum(KC$cases)+KCcases
+KC$deaths = sum(KC$deaths)+KCdeaths
 KC$afact = KC$pop10/KC$countypop #dividing the allocation factor by the four counties in the KC area
 
 NYCandKCcounties = c(36005, 36047, 36061, 36081, 36085, 29047, 29037, 29095, 29165) # removing duplicate counties from the full dataset
@@ -55,12 +55,12 @@ full3 = full2[!full2$county %in% NYCandKCcounties,]
 full3 = rbind(KC,NYC,full3)
 
 # calculating and returning case and death rates by county, per capita and per 100k
-full3$countycaserate = full3$CASES/full3$countypop 
-full3$countydeathrate = full3$DEATHS/full3$countypop
+full3$countycaserate = full3$cases/full3$countypop 
+full3$countydeathrate = full3$deaths/full3$countypop
 full3$countycaserate100k = full3$countycaserate*100000
 full3$countydeathrate100k = full3$countydeathrate*100000
 
-write.csv(full3, "CasesandDeathsbyCounty.csv")
+write.csv(full3, "casesanddeathsbycounty.csv")
 
 # calculating and returning case and death rates by hrr, per capita and per 100k 
 full4 = full3
@@ -78,4 +78,4 @@ full5$hrrdeathrate100k = full5$hrrdeathrate*100000
 full6 = distinct(full5, hrr, .keep_all = TRUE)
 full7 = full6 %>%
   select(hrr, hrrname, hrrpop, hrrcaserate, hrrdeathrate, hrrcaserate100k, hrrdeathrate100k)
-write.csv(full7, "CasesandDeathsbyHRR.csv")
+write.csv(full7, "casesanddeathsbyHRR.csv")
